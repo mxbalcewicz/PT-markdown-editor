b3385a93d604010f611b2c2bf9b1df3d28d1525e
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2021-04-19 10:26:41 +0200

    Initialize frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from './views/HomePage';
+import GlobalStyle from './theme/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme/theme';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ frontend/src/theme/GlobalStyle.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&display=swap');
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 1.6rem;
+    font-family: "Open Sans", sans-serif;
+  }
+`;
+
+export default GlobalStyle;
