335f31c0ba83f130b09e3b967c0ba08ba813c0d5
Michał Balcewicz <mxbalcewicz@gmail.com>
2021-05-10 16:12:30 +0200

    Mongodb docker changes, fixed database nestjs connection

--- backend/src/database/database.providers.ts
+++ backend/src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/pt-database?authSource=admin&w=1'),
+      mongoose.connect('mongodb://admin:adminpassword@127.0.0.1:27017/pt-database?authSource=admin'),
   },
 ];
